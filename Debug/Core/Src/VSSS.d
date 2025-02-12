7d7fc28b99c9de435ecd809d28535121f6f3b675
Thassio Silva <thxssio@gmail.com>
2025-02-11 21:32:15 -0300

    fix: rebase

--- Debug/Core/Src/VSSS.d
+++ Debug/Core/Src/VSSS.d
@@ -1,4 +1,4 @@
-Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
+Core/Src/VSSS.o: ../Core/Src/VSSS.c ../Core/Inc/VSSS.h ../Core/Inc/main.h \
  ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal.h \
  ../Core/Inc/stm32g4xx_hal_conf.h \
  ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_rcc.h \
@@ -30,9 +30,9 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
  ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_tim_ex.h \
  ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_uart.h \
  ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_uart_ex.h \
- ../Core/Inc/VSSS.h ../Core/Inc/main.h ../Core/Inc/nrf24l01p.h \
- ../Core/Inc/kinematics.h ../Core/Inc/motor_control.h \
- ../Core/Inc/encoder.h
+ ../Core/Inc/nrf24l01p.h ../Core/Inc/kinematics.h \
+ ../Core/Inc/motor_control.h ../Core/Inc/encoder.h
+../Core/Inc/VSSS.h:
 ../Core/Inc/main.h:
 ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal.h:
 ../Core/Inc/stm32g4xx_hal_conf.h:
@@ -65,8 +65,6 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
 ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_tim_ex.h:
 ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_uart.h:
 ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_uart_ex.h:
-../Core/Inc/VSSS.h:
-../Core/Inc/main.h:
 ../Core/Inc/nrf24l01p.h:
 ../Core/Inc/kinematics.h:
 ../Core/Inc/motor_control.h:
